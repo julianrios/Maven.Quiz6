44cda2bb2c4666f8020f800b4d1ae874332bbe97
Julian Rios <julianrios@zipcoders-MacBook-Pro-3.local>
2019-04-16 10:46:37 -0400

    completed generic utils

--- src/main/java/rocks/zipcode/io/generics/GenericUtils.java
+++ src/main/java/rocks/zipcode/io/generics/GenericUtils.java
@@ -1,5 +1,6 @@
 package rocks.zipcode.io.generics;
 
+import java.lang.reflect.Array;
 import java.util.Collection;
 
 public class GenericUtils {
@@ -9,6 +10,6 @@ public class GenericUtils {
      * @return array with contents identical to `collection`
      */
     public static <T> T[] toArray(Collection<T> collection) {
-        return null;
+        return (T[]) collection.toArray();
     }
 }
--- src/main/java/rocks/zipcode/io/objectorientation/Television.java
+++ src/main/java/rocks/zipcode/io/objectorientation/Television.java
@@ -1,10 +1,13 @@
 package rocks.zipcode.io.objectorientation;
 
 public class Television {
+
+
     public void turnOn() {
     }
 
     public void setChannel(Integer channel) {
+        TVChannel.getByOrdinal(channel);
     }
 
     public TVChannel getChannel() {
