090708f1929b4b9c46c753263637f23c6213826f
Julian Rios <julianrios@zipcoders-MacBook-Pro-3.local>
2019-04-16 12:53:56 -0400

    completed all methods in object orientation

--- src/main/java/rocks/zipcode/io/objectorientation/Television.java
+++ src/main/java/rocks/zipcode/io/objectorientation/Television.java
@@ -1,16 +1,22 @@
 package rocks.zipcode.io.objectorientation;
 
 public class Television {
-
+    private TVChannel currentChannel;
+    private boolean powerOn;
 
     public void turnOn() {
+        this.powerOn = true;
     }
 
     public void setChannel(Integer channel) {
-        TVChannel.getByOrdinal(channel);
+       if(this.powerOn) {
+           currentChannel = TVChannel.getByOrdinal(channel);
+       } else {
+           throw new IllegalStateException();
+       }
     }
 
     public TVChannel getChannel() {
-        return null;
+        return this.currentChannel;
     }
 }
